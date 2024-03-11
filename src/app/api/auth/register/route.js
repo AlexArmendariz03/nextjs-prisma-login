8b2a5126e0ae915d71c54f0da8a8e89301d2fe5f
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-03-11 16:46:03 -0600

    if exist  user and email

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -4,6 +4,34 @@ import db from "@/lib/db"
 export async function POST(request){
   const data = await request.json()
 
+  const userFound = await db.user.findUnique({
+    where :{
+      email: data.email
+    }
+  })
+
+  if(userFound){
+    return NextResponse.json({
+      message:"El email ya existe"
+    },{
+      status: 400
+    })
+  }
+
+  const usernameFound = await db.user.findUnique({
+    where :{
+      username: data.username
+    }
+  })
+
+  if(usernameFound){
+    return NextResponse.json({
+      message:"El usuario ya existe"
+    },{
+      status: 400
+    })
+  }
+
   console.log(data)
   const newUser = await db.user.create({
     data
